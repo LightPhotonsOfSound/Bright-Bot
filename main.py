686828e51f4e174b53cc2039661a1498546cb329
LightPhotonsOfSound <157194557+LightPhotonsOfSound@users.noreply.github.com>
2024-01-20 19:30:36 +0000

    Made changes to what is generated when you type '!help'. Added a new feature to '!translate' that shows the language of the given text. Fixed many minor errors.

--- main.py
+++ main.py
@@ -12,7 +12,8 @@ openai.api_key = os.environ['API']
 
 intents = discord.Intents.all()
 intents.messages = True
-
+global leaderboard
+leaderboard = {}
 bot = commands.Bot(command_prefix='!',intents=intents)
 
 @bot.event
@@ -34,7 +35,17 @@ async def on_message(message):
         max_tokens=100,
       )
       await message.channel.send(response.choices[0].text)   
+      
     if message.content.startswith('!translate'):
+      message_content = message.content.split(' ', 1)[1]
+      message_content = message_content.split(' ', 1)[1]
+      prompt = "Detect the language of the following text: " + message_content
+      response = openai.Completion.create(
+        model = 'gpt-3.5-turbo-instruct',
+        prompt=prompt,
+      )
+      answer = response.choices[0].text
+      await message.channel.send(f"According to our sources, this language seems to be : {answer}\n")
       if message.content.startswith('!translate spanish'):
         prompt = "Translate this into spanish: " + message.content.split('!translate spanish ')[1]
         response = openai.Completion.create(
@@ -142,7 +153,7 @@ async def on_message(message):
 
       elif message.content.startswith('!translate english'):
         text_to_translate = message.content.split('!translate english ')[1]
-        prompt = f"Translate this into English: {text_to_translate}, and tell which language the text is from"
+        prompt = f"Translate this into English: {text_to_translate}"
         response = openai.Completion.create(
             model='gpt-3.5-turbo-instruct',
             prompt=prompt,
@@ -156,8 +167,7 @@ async def on_message(message):
       
       
     if message.content.startswith('!help'):
-      await message.channel.send("***Commands***\n**!help** - Shows this message\n**!ask (question)** - Asks ChatGPT a question\n**!8ball** - Asks the magic 8ball a question to get a fun response\n**!trivia (topic)** - Generates a trivia question based on the topic\n**!answer** - Alows you to answer a trivia question**!translate (target_language) (text)** - Translates the text to the specified language\n**!funfact** - Generates a random fun fact\n**!translate (target_language) (text)** - Translates the text to the specified language and gives the english pronounciation")
-
+      await message.channel.send("***Commands***\n**!help** -     Shows this message\n**!ask (question)** - Have a doubt that needs to be answered? Use the ask function to ask a general question to BrightBot!\n**!8ball** - The function of fun has arrived! Use the 8ball function to have some fun in your study breaks and free time. Here you can input a topic you want to bot to talk about, and watch as BrightBot gives the spiciest of answers.\n**!trivia (topic)** - Are your studies too boring? Don't worry, help has arrived. Use the Trivia function to generate a trivia question on any topic of your choice. While entering the function, don't forget to specify the time limit you want to be able to answer in, which will help in increasing your thinking speed. Collaborate with your friends to double the fun!\n**!answer** - Can you think of an answer? If you can, use the answer function to answer the trivia questions and to check if your answer is right.\n**!translate** (target_language) (text) - Can't understand a certain language? Yet again, help has arrived. Use the translate function to translate text from one language to another.\n**!funfact** - Want to know more about the world? The fun fact function will generate a random fun fact that will provide you with new information!\n**Format** - The format of your function messages have to be as follows: For trivia - !trivia (topic) (time limit) For other functions - !(function) (text).\n**Note** - Do not actually use brackets while executing functions, brackets were just used to specify the type of text needed to be added. For example, if you need a trivia question on math with a time limit of 20 seconds, you have to use - !trivia math 20.")
     
     if message.content.startswith('!ask'):
         answer_prompt = message.content[4:]
@@ -178,7 +188,7 @@ async def on_message(message):
     topic = "math"
     if message.content.startswith('!answer'):
       global guess
-      guess = message.content.lower()[7]
+      guess = message.content.lower()[8]
       print(f"{message.author} guessed: {guess}")
       global guesser
       guesser = message.author
@@ -187,50 +197,60 @@ async def on_message(message):
 
     if message.content.startswith('!trivia'):
       responses.clear()
-      if len(message.content) > 6:
-        topic = message.content.split('!trivia ')[1]
-        prompt1 = f'Create a multiple-choice question, (make only 4 options) on the topic of "{topic}" and dont give the answer with the question.'
+      if len(message.content) > 10:
+        time = message.content[-2:]
+        time = int(time)
+        if time < 10 or time > 40:
+          await message.channel.send("The time must be between 10 and 40 seconds! Try again.")
+        else:
+          topic = message.content[8:-2]
+          prompt1 = f'Create a multiple-choice question, (make only 4 options) on the topic of "{topic}" and dont give the answer with the question.'
+  
+          response = openai.Completion.create(
+              model ='gpt-3.5-turbo-instruct',
+              prompt=prompt1,
+              max_tokens=150,
+            # chat_completion=True
+          ) 
+  
+          # Extract the generated question from the response
+          generated_question = response['choices'][0]['text'].strip()
+  
+  
+  
+          # Create a dynamic prompt for the answer
+          answer_prompt = f"This is a multiple choice question : {generated_question}\nProvide an answer with some explaination Example :' A. .. ' Remember, the answer has to first start with the correct letter and then go to the rest. "
+  
+          # Request an answer based on the generated question
+          answer_response = openai.Completion.create(
+              model ='gpt-3.5-turbo-instruct',
+              prompt=answer_prompt,
+              max_tokens=400,
+             # chat_completion=True
+          )
+  
+          # Extract the generated answer from the response
+          generated_answer = answer_response['choices'][0]['text'].strip()
+          correct_option = generated_answer.lower()[0]
+          print(f"Correct option: {correct_option}")
 
-        response = openai.Completion.create(
-            model ='gpt-3.5-turbo-instruct',
-            prompt=prompt1,
-            max_tokens=150,
-          # chat_completion=True
-        ) 
+         # if message.content.startswith('!leaderboard'):
+            
+  
+          # Send the generated question and answer to the Discord channel
+          await message.channel.send(f"You have {time} seconds to answer this: {generated_question}")
+          await asyncio.sleep(time)
+  
+          await message.channel.send(f"The answer is : {generated_answer}")
+          print(responses)
+          val_list = list(responses.values())
+          key_list = list(responses.keys())
+          ind = val_list.index(correct_option)
+          username = key_list[ind]
+          print(responses)
+          await message.channel.send(f"\n***{guesser} guessed it right!!***")
 
-        # Extract the generated question from the response
-        generated_question = response['choices'][0]['text'].strip()
-
-
-
-        # Create a dynamic prompt for the answer
-        answer_prompt = f"This is a multiple choice question : {generated_question}\nProvide an answer with some explaination Example :' A. .. ' "
-
-        # Request an answer based on the generated question
-        answer_response = openai.Completion.create(
-            model ='gpt-3.5-turbo-instruct',
-            prompt=answer_prompt,
-            max_tokens=400,
-           # chat_completion=True
-        )
-      #  await asyncio.sleep(15)
-
-        # Extract the generated answer from the response
-        generated_answer = answer_response['choices'][0]['text'].strip()
-        correct_option = generated_answer.lower()[0]
-        print(f"Correct option: {correct_option}")
-
-        # Send the generated question and answer to the Discord channel
-        await message.channel.send(f"You have 20 seconds to answer this: {generated_question}")
-        await asyncio.sleep(20)
-
-        await message.channel.send(f"The answer is : {generated_answer}")
-        print(responses)
-        val_list = list(responses.values())
-        key_list = list(responses.keys())
-        ind = val_list.index(correct_option)
-        username = key_list[ind]
-        await message.channel.send(f"\n***{username} guessed it right!!***")
+          
 
 
 
